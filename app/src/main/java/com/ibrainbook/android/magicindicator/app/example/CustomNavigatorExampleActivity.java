2f355f562ff8ee14b2637a311d6a3711907a5220
Robert Han <brainbook.cc@outlook.com>
2022-04-03 19:22:23 -0700

    Initial commit

---
+++ app/src/main/java/com/ibrainbook/android/magicindicator/app/example/CustomNavigatorExampleActivity.java
@@ -0,0 +1,85 @@
+package com.ibrainbook.android.magicindicator.app.example;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ibrainbook.android.magicindicator.MagicIndicator;
+import com.ibrainbook.android.magicindicator.ViewPagerHelper;
+import com.ibrainbook.android.magicindicator.app.ext.navigator.ScaleCircleNavigator;
+import com.ibrainbook.android.magicindicator.buildins.circlenavigator.CircleNavigator;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.ibrainbook.android.magicindicator.app.R;
+
+public class CustomNavigatorExampleActivity extends AppCompatActivity {
+    private static final String[] CHANNELS = new String[]{"CUPCAKE", "DONUT", "ECLAIR", "GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_navigator_example_layout);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mExamplePagerAdapter);
+
+        initMagicIndicator1();
+        initMagicIndicator2();
+        initMagicIndicator3();
+    }
+
+    private void initMagicIndicator1() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator1);
+        CircleNavigator circleNavigator = new CircleNavigator(this);
+        circleNavigator.setCircleCount(CHANNELS.length);
+        circleNavigator.setCircleColor(Color.RED);
+        circleNavigator.setCircleClickListener(new CircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(circleNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+    private void initMagicIndicator2() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator2);
+        CircleNavigator circleNavigator = new CircleNavigator(this);
+        circleNavigator.setFollowTouch(false);
+        circleNavigator.setCircleCount(CHANNELS.length);
+        circleNavigator.setCircleColor(Color.RED);
+        circleNavigator.setCircleClickListener(new CircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(circleNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+    private void initMagicIndicator3() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator3);
+        ScaleCircleNavigator scaleCircleNavigator = new ScaleCircleNavigator(this);
+        scaleCircleNavigator.setCircleCount(CHANNELS.length);
+        scaleCircleNavigator.setNormalCircleColor(Color.LTGRAY);
+        scaleCircleNavigator.setSelectedCircleColor(Color.DKGRAY);
+        scaleCircleNavigator.setCircleClickListener(new ScaleCircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(scaleCircleNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+}
